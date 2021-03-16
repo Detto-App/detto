e2181af54ede2cdd43297ce1e116afe9e997ef22
Vinay Hiremath <vinayhiremath288@gmail.com>
2021-03-16 19:47:08 +0530

    added createbtn

--- app/src/main/java/com/dettoapp/detto/TeacherActivity/Dialog/GroupInfoDialog.kt
+++ app/src/main/java/com/dettoapp/detto/TeacherActivity/Dialog/GroupInfoDialog.kt
@@ -3,12 +3,19 @@ package com.dettoapp.detto.TeacherActivity.Dialog
 import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
+import android.widget.Button
 import com.dettoapp.detto.R
 
-class GroupInfoDialog (context:Context):Dialog(context){
+class GroupInfoDialog (context:Context):Dialog(context,android.R.style.ThemeOverlay){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dialog_groupinfo)
+        val btn=findViewById<Button>(R.id.btn_createclassroom)
+        btn.setOnClickListener {
+            dismiss()
+        }
     }
+
+
 
 }
