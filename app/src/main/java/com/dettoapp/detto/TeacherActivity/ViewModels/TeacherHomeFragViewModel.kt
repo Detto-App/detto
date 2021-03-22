0b97241a08a4464653159871b3aa715e520a90c2
Vishwa-Raghavendra <42895647+Vishwa-Raghavendra@users.noreply.github.com>
2021-03-22 22:52:04 +0530

    Classroom creation code clean up

--- app/src/main/java/com/dettoapp/detto/TeacherActivity/ViewModels/TeacherHomeFragViewModel.kt
+++ app/src/main/java/com/dettoapp/detto/TeacherActivity/ViewModels/TeacherHomeFragViewModel.kt
@@ -1,7 +1,7 @@
 package com.dettoapp.detto.TeacherActivity.ViewModels
 
+import android.annotation.SuppressLint
 import android.content.Context
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -9,34 +9,33 @@ import androidx.lifecycle.viewModelScope
 import com.dettoapp.detto.Models.Classroom
 import com.dettoapp.detto.TeacherActivity.TeacherRepository
 import com.dettoapp.detto.UtilityClasses.Resource
-import com.dettoapp.detto.UtilityClasses.RetrofitInstance
 import com.dettoapp.detto.UtilityClasses.Utility
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.lang.Exception
 
-class TeacherHomeFragViewModel(private val repository: TeacherRepository, private val context: Context) : ViewModel() {
+@SuppressLint("StaticFieldLeak")
+class TeacherHomeFragViewModel(
+    private val repository: TeacherRepository,
+     private val context: Context
+) : ViewModel() {
 
-    private val _teacher = MutableLiveData<Resource<String>>()
-    val teacher: LiveData<Resource<String>>
-        get() = _teacher
+    private val _classRoomCreation = MutableLiveData<Resource<String>>()
+    val classRoomCreation: LiveData<Resource<String>>
+        get() = _classRoomCreation
 
 
     fun classRoomData(classroomName: String, sem: String, sec: String) {
-        val uid=repository.getUid(context)
-        val classroom = Classroom(classroomName, sem, sec, Utility.createID(),uid)
         viewModelScope.launch(Dispatchers.IO) {
             try {
-                _teacher.postValue(Resource.Loading())
-                repository.createClassroom(context,classroom)
-                _teacher.postValue(Resource.Success(data = "classroom created successfully"))
-            }catch (e:Exception ){
-                Log.d("teachermodel",e.localizedMessage)
-                _teacher.postValue(Resource.Error(message = e.localizedMessage))
+                _classRoomCreation.postValue(Resource.Loading())
+                val uid = repository.getUid(context)
+                val classroom = Classroom(classroomName, sem, sec, Utility.createID(), uid)
+                repository.createClassroom(context, classroom)
+                _classRoomCreation.postValue(Resource.Success(data = "classroom created successfully"))
+            } catch (e: Exception) {
+                _classRoomCreation.postValue(Resource.Error(message = ""+e.localizedMessage))
             }
-//            try {
-//                RetrofitInstance.createClassroomAPI.createClassroom(classroom)
-//            } catch (e: Exception) { }
         }
     }
 }
