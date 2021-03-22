f067418ff9aa8434bafa3d7c1d414a6c030037df
vikas <vikasainapur@gmail.com>
2021-03-22 22:25:14 +0530

    local sql db try

--- app/src/main/java/com/dettoapp/detto/Models/Classroom.kt
+++ app/src/main/java/com/dettoapp/detto/Models/Classroom.kt
@@ -1,3 +1,7 @@
 package com.dettoapp.detto.Models
 
-class Classroom(val classroomname: String, val year: String, val section: String, val classroomuid: String, val userid: String)
+//import androidx.room.Entity
+
+//@Entity
+data class Classroom(val classroomname: String, val year: String, val section: String, val classroomuid: String, val userid: String)
+//kapt "androidx.lifecycle:lifecycle-compiler:$lifecycle_version"
